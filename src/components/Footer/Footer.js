5a17c3daa59c7061b8c1d17a9abafef8644ada8c
Emily Lauth <88945874+emilylauth@users.noreply.github.com>
2022-02-10 23:02:11 -0600

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/emilylauth/emilylauth.github.io'
+      className='link footer__link'
+    >
+      Check out the repository for this portfolio
+    </a>
+  </footer>
+)
+
+export default Footer
